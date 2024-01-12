2051d784fe7c744d0c6019fc8228eac02ca264a8
Nitish Singh <153308784+Nitish-Singh10@users.noreply.github.com>
2024-01-12 14:53:47 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,6 +3,7 @@
     $mysqli = new mysqli("62.72.28.52","u114668675_swadeshi","Nitish@69","u114668675_swadeshi");
 
     if($mysqli->connect_error)
+        // gygbgbgbygbgyb
     {
         die('Connection Failed'.$mysqli->connect_error);
     }
