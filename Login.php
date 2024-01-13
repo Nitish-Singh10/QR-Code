96335cc79ac6b1705ebd15c90bd766e8e934ada4
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-01-13 20:10:17 +0530

    login

--- Login.php
+++ Login.php
@@ -3,39 +3,45 @@ session_start();
 include("config.php");
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    $Email = $_POST['Email'];
-    $Password = $_POST['Password'];
+    $email = $_POST['Email'];
+    $password = $_POST['Password'];
 
-    if (!empty($Email) && !empty($Password) && !is_numeric($Email)) {
-        $query = "SELECT * FROM admin WHERE Email = ? LIMIT 1";
-        $stmt = mysqli_prepare($mysqli, $query);
-
-        if ($stmt) {
-            mysqli_stmt_bind_param($stmt, "s", $Email);
-            mysqli_stmt_execute($stmt);
-
-            $result = mysqli_stmt_get_result($stmt);
-
-            if ($result && mysqli_num_rows($result) > 0) {
-                $user_data = mysqli_fetch_assoc($result);
-
-                if (password_verify($Password, $user_data['Password'])) {
-                    $_SESSION['user_name'] = $user_data['Fname'];
-                    header("location: admin.php");
-                    die;
-                } else {
-                    echo "<script>alert('Wrong Password');</script>";
-                }
-            } else {
-                echo "<script>alert('Wrong User Name');</script>";
-            }
-
-            mysqli_stmt_close($stmt);
-        } else {
-            echo "<script>alert('Error in preparing statement');</script>";
-        }
-    } else {
+    // Validate input
+    if (empty($email) || empty($password) || is_numeric($email)) {
         echo "<script>alert('Invalid Input');</script>";
+        exit;
+    }
+
+    // Prepare and execute the SQL query
+    $query = "SELECT * FROM admin WHERE Email = ? LIMIT 1";
+    $stmt = mysqli_prepare($mysqli, $query);
+
+    if ($stmt) {
+        mysqli_stmt_bind_param($stmt, "s", $email);
+        mysqli_stmt_execute($stmt);
+
+        $result = mysqli_stmt_get_result($stmt);
+
+        if ($result && $user_data = mysqli_fetch_assoc($result)) {
+            // Compare passwords directly (assuming plain text)
+            if ($user_data['Password'] === $password) {
+                $_SESSION['user_name'] = $user_data['Fname'];
+                $_SESSION['type'] = $user_data['type']; 
+                $_SESSION['no'] = $user_data['no']; 
+
+
+                header("location: admin.php");
+                exit;
+            } else {
+                echo "<script>alert('Wrong User Name or Password');</script>";
+            }
+        } else {
+            echo "<script>alert('Wrong User Name');</script>";
+        }
+
+        mysqli_stmt_close($stmt);
+    } else {
+        echo "<script>alert('Error in preparing statement');</script>";
     }
 }
 ?>
--- admin.php
+++ admin.php
@@ -2,76 +2,83 @@
 include("config.php");
 session_start();
 
+if (isset($_SESSION['user_name'])) {
+    // Welcome message and logout link
+    echo '<!DOCTYPE html>
+          <html lang="en">
+          <head>
+              <meta charset="UTF-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+              <title>Admin Page</title>
+          </head>
+          <body>
+              <h3>Welcome ' . $_SESSION['user_name'] . '</h3>
+              <p><a href="Logout.php">Log Out</a></p>
+
+              <div class="col-md-2"></div>
+              <div class="col-md-8">
+                  <h1>QR Code List</h1><br><br>
+                  <table class="table table-bordered table-hover">
+                      <thead>
+                          <tr>
+                              <th>Sr.No</th>
+                              <th>Name</th>
+                              <th>Designation</th>
+                              <th>Name of Company</th>
+                              <th>Address of Company</th>
+                              <th>Phone</th>
+                              <th>Email</th>
+                              <th>QR CODE</th>
+                          </tr>
+                      </thead>
+                      <tbody>';
+
+    // Switch based on session data
+    switch ($_SESSION['type']) {
+        case 'admin':
+            $query = "SELECT * FROM registration";
+            break;
+        case 'registrationdesk':
+            $pcid = $_SESSION['no'];
+            $query = "SELECT * FROM registration WHERE pcid = $pcid";
+
+            break;
+        default:
+            // Handle default case or exit gracefully
+            exit("Invalid session data");
+    }
+    echo $query;
+
+    // Execute and display table rows
+    if ($result = $mysqli->query($query)) {
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_object()) {
+                echo '<tr>
+                          <td>' . $row->Id . '</td>
+                          <td>' . $row->Name . '</td>
+                          <td>' . $row->Designation . '</td>
+                          <td>' . $row->NOC . '</td>
+                          <td>' . $row->AOC . '</td>
+                          <td>' . $row->Phone . '</td>
+                          <td>' . $row->Email . '</td>
+                          <td><img src="' . $row->OR . '" width="50px" height="50px"></td>
+                          <td><a href="Try.php">Download QR</a></td>
+                      </tr>';
+            }
+        }
+    }
+
+    // Close HTML tags
+    echo '        </tbody>
+                  </table>
+              </div>
+              <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+              <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+          </body>
+          </html>';
+} else {
+    header("location: login.php");
+    exit;
+}
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-    <title>Admin Page</title>
-</head>
-<body>
-    <h3>Welcome Admin</h3>
-    <p><a href="Login.php">Log Out</a></p>
-
-
-    <div class="col-md-2"></div>
-    <div class="col-md-8">
-            <h1>QR Code List</h1>
-            <br>   
-           
-            <br>
-            <table class="table table-bordered table-hover">
-                <thead>
-                    <tr>
-                        <th>Sr.No</th>
-                        <th>Name</th>
-                        <th>Designation</th>
-                        <th>Name of Company</th>
-                        <th>Address of Company</th>
-                        <th>Phone</th>
-                        <th>Email</th>
-                        <th>QR CODE</th>
-                      
-                    </tr>
-                </thead>
-                <tbody>
-                  <?php
-                    require_once('config.php');
-                   
-                   $query="Select * from registration";
-                    
-                    if($result = $mysqli->query($query)){
-                        if($result->num_rows > 0){
-                            while($row = $result->fetch_object()){
-                                
-                    ?>
-                        <tr>
-                                <td><?php echo $row->Id; ?></td>
-                                <td><?php echo $row->Name; ?></td>
-                                <td><?php echo $row->Designation ?></td>
-                                <td><?php echo $row->NOC ?></td>
-                                <td><?php echo $row->AOC ?></td>
-                                <td><?php echo $row->Phone ?></td>
-                                <td><?php echo $row->Email ?></td>
-                                <td><img src='<?php echo $row->OR; ?>' width="50px" height="50px"></td>
-                                <td><a href="Try.php">Download QR</a></td>
-                        </tr>   
-                        
-
-                 <?php         
-                            }
-                       } 
-                    }     
-                  ?>
-                </tbody>
-            </table>
-            
-    </div>          
-        <!-- jQuery -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <!-- Bootstrap JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</body>
-</html>
