267a64dbb82d000b746992a89bd21a3cb4cd74e2
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-01-15 14:09:10 +0530

    Update admin.php

--- admin.php
+++ admin.php
@@ -3,44 +3,49 @@ include("config.php");
 session_start();
 
 if (isset($_SESSION['user_name'])) {
-    // Welcome message and logout link
     echo '<!DOCTYPE html>
           <html lang="en">
           <head>
               <meta charset="UTF-8">
               <meta name="viewport" content="width=device-width, initial-scale=1.0">
               <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+
+              <!-- DataTables CSS -->
+              <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+
+              <!-- Select2 CSS -->
+              <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+
               <title>Admin Page</title>';
               include "./script.php";
-
               
-    echo      '</head>
+    echo '</head>
           <body>';
           include "./script.php";
 
+    echo '<h3>Welcome ' . $_SESSION['user_name'] . '</h3>
+          <p><a href="Logout.php">Log Out</a></p>
 
-          echo   '<h3>Welcome ' . $_SESSION['user_name'] . '</h3>
-              <p><a href="Logout.php">Log Out</a></p>
+          <div class="col-md-2"></div>
+          <div class="col-md-8">
+              <h1>QR Code List</h1><br><br>
+              <div id="columnControls"></div>
+              <table id="data" class="table table-bordered table-hover">
+                  <thead>
+                      <tr>
+                          <th>Sr.No</th>
+                          <th>Name</th>
+                          <th>Designation</th>
+                          <th>Name of Company</th>
+                          <th>Address of Company</th>
+                          <th>Phone</th>
+                          <th>Email</th>
+                          <th>QR CODE</th>
+                          <th>Download QR</th>
+                      </tr>
+                  </thead>
+                  <tbody>';
 
-              <div class="col-md-2"></div>
-              <div class="col-md-8">
-                  <h1>QR Code List</h1><br><br>
-                  <table id="data" class="table table-bordered table-hover">
-                      <thead>
-                          <tr>
-                              <th>Sr.No</th>
-                              <th>Name</th>
-                              <th>Designation</th>
-                              <th>Name of Company</th>
-                              <th>Address of Company</th>
-                              <th>Phone</th>
-                              <th>Email</th>
-                              <th>QR CODE</th>
-                          </tr>
-                      </thead>
-                      <tbody>';
-
-    // Switch based on session data
     switch ($_SESSION['type']) {
         case 'admin':
             $query = "SELECT * FROM registration";
@@ -48,15 +53,11 @@ if (isset($_SESSION['user_name'])) {
         case 'registrationdesk':
             $pcid = $_SESSION['no'];
             $query = "SELECT * FROM registration WHERE pcid = $pcid";
-
             break;
         default:
-            // Handle default case or exit gracefully
             exit("Invalid session data");
     }
-    // echo $query;
 
-    // Execute and display table rows
     if ($result = $mysqli->query($query)) {
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_object()) {
@@ -75,14 +76,36 @@ if (isset($_SESSION['user_name'])) {
         }
     }
 
-    // Close HTML tags
-    echo '        </tbody>
-                  </table>
-              </div>
-              <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-              <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-          </body>
-          </html>';
+    echo '</tbody>
+          </table>
+      </div>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+      <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+      <script>
+          $(document).ready(function() {
+              // Initialize DataTable
+              var table = $("#data").DataTable({
+                  searching: true,
+                  paging: true,
+                  ordering: true,
+                  buttons: [
+                      {
+                          extend: "colvis",
+                          postfixButtons: ["colvisRestore"],
+                          collectionLayout: "fixed two-column"
+                      }
+                  ],
+                  dom: "Bfrtip" // Controls position: B = Buttons, f = Filtering input, r = Processing display element, t = The table, i = Information summary
+              });
+
+              // Add the controls to the desired location
+              table.buttons().container().appendTo($("#columnControls"));
+          });
+      </script>
+      </body>
+      </html>';
 } else {
     header("location: login.php");
     exit;
