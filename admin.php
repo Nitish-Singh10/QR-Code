23ece42ef3391559d91a8589606b94eef00db7e4
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-01-15 14:18:36 +0530

    Update admin.php

--- admin.php
+++ admin.php
@@ -17,7 +17,7 @@ if (isset($_SESSION['user_name'])) {
               <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 
               <title>Admin Page</title>';
-              include "./script.php";
+            //   include "./script.php";
               
     echo '</head>
           <body>';
