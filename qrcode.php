c0dc5250b0f80da76eff3b805d06193ad4e5feab
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-01-16 12:02:57 +0530

    Update qrcode.php

--- qrcode.php
+++ qrcode.php
@@ -1,42 +1,73 @@
 <?php
 
-    include('phpqrcode/qrlib.php');
-    require_once('config.php');
-    
-    // Getting Values from Form
-    $Name = $_POST['Name'];
-    $Designation = $_POST['Designation'];
-    $NOC = $_POST['NOC'];
-    $Address = $_POST['Address'];
-    $Phone = $_POST['Phone'];
-    $Email = $_POST['Email'];
+include('phpqrcode/qrlib.php');
+require_once('config.php');
 
-    $pname= "Name:- $Name.".`<br>`.
-    "Designation:- $Desigination".`<br>`.
-    "Name of Company:- $NOC.".`<br>`.
-    "Address:- $Address.".`<br>`.
-    "Phone:- $Phone.".`<br>`.
-    "Email:- $Email";
-    
+function generateUniqueId($length = 5) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $maxAttempts = 10; // Maximum attempts to generate a unique ID
+
+    for ($attempt = 0; $attempt < $maxAttempts; $attempt++) {
+        $randomString = '';
+
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, strlen($characters) - 1)];
+        }
+
+        // Combine timestamp and random string to create a unique ID
+        $timestamp = time();
+        $uniqueId = $timestamp . $randomString;
+
+        // Check if the generated ID already exists in the database
+        $queryCheck = "SELECT * FROM registration WHERE UniqueID = '$uniqueId'";
+        $resultCheck = $mysqli->query($queryCheck);
+
+        if ($resultCheck->num_rows === 0) {
+            return $uniqueId; // Return the unique ID if it's not found in the database
+        }
+    }
+
+    // If maximum attempts reached, throw an error or handle it as needed
+    throw new Exception("Unable to generate a unique ID after $maxAttempts attempts");
+}
+
+// Getting Values from Form
+$Name = $_POST['Name'];
+$Designation = $_POST['Designation'];
+$NOC = $_POST['NOC'];
+$Address = $_POST['Address'];
+$Phone = $_POST['Phone'];
+$Email = $_POST['Email'];
+
+// Generate a unique ID using the function
+try {
+    $uniqueId = generateUniqueId();
+
+    // Check uniqueness of email and phone
+    $queryCheck = "SELECT * FROM registration WHERE Email = '$Email' OR Phone = '$Phone' OR UniqueID = '$uniqueId'";
+    $resultCheck = $mysqli->query($queryCheck);
+
+    if ($resultCheck->num_rows > 0) {
+        header('location:index.php?msg=Email, Phone, or ID already exists');
+        exit();
+    }
+
+    $pname = $uniqueId;
     $path = 'image/';
-    $file = $path.$Name.".png";
+    $file = $path . $Name . ".png";
 
     $ecc = 'L';
     $pixel_Size = 10;
     $frame_Size = 10;
 
-    $query = "INSERT INTO registration VALUES('','$Name','$Designation','$NOC','$Address','$Phone','$Email','$file')";
-    // mysqli_query($mysqli,$query);
-    if($mysqli->query($query)==true)
-    {
-        Qrcode::png($pname,$file,$ecc,$pixel_Size,$frame_Size);
+    $query = "INSERT INTO registration VALUES('', '$Name', '$Designation', '$NOC', '$Address', '$Phone', '$Email', '$file','$uniqueId')";
+    if ($mysqli->query($query) === true) {
+        Qrcode::png($pname, $file, $ecc, $pixel_Size, $frame_Size);
         header('location:index.php?msg=data added successfully');
+    } else {
+        header('location:index.php?msg=data failed');
     }
-    else{
-        header('location:index.php?msg=data failed ');
-    }
-    
-   
-
-    
+} catch (Exception $e) {
+    header('location:index.php?msg=' . $e->getMessage());
+}
 ?>
